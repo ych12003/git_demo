7160a3ff6c1aa5e0c22b779e09bd313cdc9ce7d1
ych12003 <ych12003@163.com>
2014-08-13 17:25:22 +0800

    first commit by ych12003

---
+++ com.sohu.githubtest/src/main/java/com/sohu/app/com/sohu/githubtest/App.java
@@ -0,0 +1,14 @@
+package com.sohu.app.com.sohu.githubtest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("This is a test");
+    }
+}
